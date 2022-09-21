a6bd308c8cb204fc002132d5a0f4b7f254a95056
金条k <11596928+sansjtw@user.noreply.gitee.com>
2022-09-21 10:26:38 +0000

    删除文件 use/6.py

--- use/6.py
+++
@@ -1,9 +0,0 @@
-import requests
-url="https://fanyi.baidu.com/sug"
-s=input("请输入要翻译的英文单词或中文:  ")
-dat={
-	"kw":s
-	}
-resp=requests.post(url,data=dat)#发送post请求，发送的数据必须放在字典中，通过data参数进行传递
-print(resp.json()) #将服务器返回的内容直接处理成json => dict
-resp.close()
